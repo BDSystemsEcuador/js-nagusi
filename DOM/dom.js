1fe5bf2bf44402e3b5016177a11f897df6663be2
Byron Delgado <byrondanipm@gmail.com>
2021-06-27 01:01:19 +0000

    first commit

---
+++ DOM/dom.js
@@ -0,0 +1,12 @@
+// console.log(window);
+// console.log(document);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.doctype);
+// console.log(document.charset);
+// console.log(document.links);
+// console.log(document.images);
+// console.log(document.scripts);
+// console.log(document.styleSheets);
+// console.log(document.forms);
